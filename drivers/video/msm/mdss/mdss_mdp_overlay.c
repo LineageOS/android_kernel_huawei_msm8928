02595e5a40f3f4d301ea21d40a811382a0b0e084
Mayank Chopra <makchopra@codeaurora.org>
2013-12-19 10:46:04 +0530

    msm: mdss: Use MDP_SOLID_FILL to enable color on pipe
    To enable constant color on a pipe, set MDP_SOLID_FILL in pipe
    flag. This will do a solid fill on the pipe with the color
    specified in bg_color.
    
    CRs-Fixed:592032
    Change-Id: I3201e2fa657a6fc6d0e7f51f8585366afc3d29a3
    Signed-off-by: Mayank Chopra <makchopra@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -616,7 +616,6 @@ static int mdss_mdp_overlay_pipe_setup(struct msm_fb_data_type *mfd,
 	}
 
 	pipe->params_changed++;
-	pipe->has_buf = 0;
 
 	req->vert_deci = pipe->vert_deci;
 
@@ -1277,6 +1276,9 @@ static int mdss_mdp_overlay_queue(struct msm_fb_data_type *mfd,
 
 	pr_debug("ov queue pnum=%d\n", pipe->num);
 
+	if (pipe->flags & MDP_SOLID_FILL)
+		pr_warn("Unexpected buffer queue to a solid fill pipe\n");
+
 	flags = (pipe->flags & MDP_SECURE_OVERLAY_SESSION);
 	flags |= (pipe->flags & MDP_SECURE_DISPLAY_OVERLAY_SESSION);
 
@@ -1294,7 +1296,6 @@ static int mdss_mdp_overlay_queue(struct msm_fb_data_type *mfd,
 	if (IS_ERR_VALUE(ret)) {
 		pr_err("src_data pmem error\n");
 	}
-	pipe->has_buf = 1;
 	mdss_mdp_pipe_unmap(pipe);
 
 	return ret;
@@ -1566,7 +1567,6 @@ static void mdss_mdp_overlay_pan_display(struct msm_fb_data_type *mfd,
 	else
 		buf->p[0].len = fbi->fix.smem_len - offset;
 	buf->num_planes = 1;
-	pipe->has_buf = 1;
 	mdss_mdp_pipe_unmap(pipe);
 
 	if (fbi->var.xres > MAX_MIXER_WIDTH || mfd->split_display) {
@@ -1584,7 +1584,6 @@ static void mdss_mdp_overlay_pan_display(struct msm_fb_data_type *mfd,
 			pipe_ndx[1] = pipe->ndx;
 
 		pipe->back_buf = *buf;
-		pipe->has_buf = 1;
 		mdss_mdp_pipe_unmap(pipe);
 	}
 	mutex_unlock(&mdp5_data->ov_lock);
