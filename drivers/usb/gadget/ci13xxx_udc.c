559f423b4632719ac8d1bc15a6842ffec394390b
Manu Gautam <mgautam@codeaurora.org>
2014-06-25 12:12:53 +0530

    USB: ci13xxx_udc:  Add support for UI interrupts counting
    Add counting of UI or USB transfers completion interrupts
    so that it can be used to determine USB transfers or
    BUS usage. This is a simple approach which gives
    reasonable accuracy to avoid counting actual bytes
    that got transferred over USB.
    
    Change-Id: I1ee3c6997e40ee5db1da26af1e68fced06ea2de4
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>

--- drivers/usb/gadget/ci13xxx_udc.c
+++ drivers/usb/gadget/ci13xxx_udc.c
@@ -2424,6 +2424,7 @@ static int _gadget_stop_activity(struct usb_gadget *gadget)
 	udc->configured = 0;
 	spin_unlock_irqrestore(udc->lock, flags);
 
+	gadget->xfer_isr_count = 0;
 	gadget->b_hnp_enable = 0;
 	gadget->a_hnp_support = 0;
 	gadget->host_request = 0;
@@ -3834,6 +3835,7 @@ static irqreturn_t udc_irq(void)
 			ci13xxx_udc_set_enum_flag();
 #endif
 			isr_statistics.ui++;
+			udc->gadget.xfer_isr_count++;
 			isr_tr_complete_handler(udc);
 		}
 		if (USBi_SLI & intr) {
