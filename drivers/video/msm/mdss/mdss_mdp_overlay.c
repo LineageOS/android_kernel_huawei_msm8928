ee96e8b97d5cdc3ab6eb83fc6b94b1cea8220411
Jurica Vukadin <u.ra604@gmail.com>
2015-06-16 14:23:58 +0200

    Revert "Temp hax: get the display to work"
    This reverts commit 99b5c391656de243a240da48a0600aadca664075.

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -2589,7 +2589,7 @@ static int mdss_mdp_overlay_on(struct msm_fb_data_type *mfd)
 		mdp5_data->ctl = ctl;
 	}
 
-	if (1 /*!mfd->panel_info->cont_splash_enabled*/ &&
+	if (!mfd->panel_info->cont_splash_enabled &&
 		(mfd->panel_info->type != DTV_PANEL) &&
 		(mfd->panel_info->type != WRITEBACK_PANEL)) {
 		rc = mdss_mdp_overlay_start(mfd);
