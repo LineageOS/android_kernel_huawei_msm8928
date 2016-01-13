020cf176cbe35e9c885570a9fc53ce59260ff47e
Jurica Vukadin <u.ra604@gmail.com>
2016-01-13 10:34:54 +0100

    msm: camera: Remove camera wakelock
    There is no evidence to suggest it does anything useful for us, so just
    get rid of it.
    
    Change-Id: If688c41f787a3764b5c76c369b6dff10960e0f4f

--- drivers/media/platform/msm/camera_v2/camera/camera.c
+++ drivers/media/platform/msm/camera_v2/camera/camera.c
@@ -27,9 +27,6 @@
 #include <linux/iommu.h>
 #include <linux/platform_device.h>
 #include <media/v4l2-fh.h>
-#ifdef CONFIG_HUAWEI_KERNEL_CAMERA
-#include <linux/wakelock.h>
-#endif
 
 #include "camera.h"
 #include "msm.h"
@@ -44,11 +41,6 @@ struct camera_v4l2_private {
 	unsigned int is_vb2_valid; /*0 if no vb2 buffers on stream, else 1*/
 	struct vb2_queue vb2_q;
 };
-
-#ifdef CONFIG_HUAWEI_KERNEL_CAMERA
-static struct wake_lock cam_wakelock;
-static int cam_wakelock_init = 0;
-#endif
 
 static void camera_pack_event(struct file *filep, int evt_id,
 	int command, int value, struct v4l2_event *event)
@@ -641,16 +633,6 @@ static int camera_v4l2_close(struct file *filep)
 	atomic_sub_return(1, &pvdev->opened);
 
 	if (atomic_read(&pvdev->opened) == 0) {
-#ifdef CONFIG_HUAWEI_KERNEL_CAMERA
-		if(1 == cam_wakelock_init && !wake_lock_active(&cam_wakelock)) {
-			pr_info("%s: start camera wake_lock_timeout!\n",__func__);
-			//wake lock 500ms for camera exit
-			wake_lock_timeout(&cam_wakelock, HZ/2);
-		}
-		else
-			pr_info("%s: do not need wake_lock now, cam_wakelock_init = %d\n",
-					__func__, cam_wakelock_init);
-#endif
 
 		camera_pack_event(filep, MSM_CAMERA_SET_PARM,
 			MSM_CAMERA_PRIV_DEL_STREAM, -1, &event);
@@ -781,12 +763,6 @@ int camera_init_v4l2(struct device *dev, unsigned int *session)
 	atomic_set(&pvdev->stream_cnt, 0);
 	video_set_drvdata(pvdev->vdev, pvdev);
 	device_init_wakeup(&pvdev->vdev->dev, 1);
-#ifdef CONFIG_HUAWEI_KERNEL_CAMERA
-	if(!cam_wakelock_init) {
-		cam_wakelock_init = 1;
-		wake_lock_init(&cam_wakelock, WAKE_LOCK_SUSPEND, "cam_wakelock");
-	}
-#endif
 	goto init_end;
 
 video_register_fail:
